aa6e8cdb7e00b2b297463683fb572e4bdfaed035
TatyanaPilke <kykla.mc2@gmail.com>
2021-07-17 18:49:42 +0300

    added section products

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,8 @@ let path = {
 		css: project_name + "/css/",
 		images: project_name + "/img/",
 		fonts: project_name + "/fonts/",
-		videos: project_name + "/videos/"
+		videos: project_name + "/videos/",
+		json: project_name + "/json/"
 	},
 	src: {
 		favicon: src_folder + "/img/favicon.{jpg,png,svg,gif,ico,webp}",
@@ -43,13 +44,15 @@ let path = {
 		css: src_folder + "/scss/style.scss",
 		images: [src_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}", "!**/favicon.*"],
 		fonts: src_folder + "/fonts/*.ttf",
-		videos: src_folder + "/videos/*.*"
+		videos: src_folder + "/videos/*.*",
+		json: src_folder + "/json/*.*"
 	},
 	watch: {
 		html: src_folder + "/**/*.html",
 		js: src_folder + "/**/*.js",
 		css: src_folder + "/scss/**/*.scss",
-		images: src_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}"
+		images: src_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+		json: src_folder + "/json/*.*"
 	},
 	clean: "./" + project_name + "/"
 };
@@ -61,6 +64,11 @@ function browserSync(done) {
 		notify: false,
 		port: 3000,
 	});
+}
+function json() {
+	return src(path.src.json)
+		.pipe(plumber())
+		.pipe(dest(path.build.json))
 }
 function html() {
 	return src(path.src.html, {})
@@ -197,6 +205,73 @@ function fontstyle() {
 	}
 }
 
+/*
+function checkWeight(fontname) {
+  let weight = 400;
+  switch (true) {
+	 case /Thin/.test(fontname):
+		weight = 100;
+		break;
+	 case /ExtraLight/.test(fontname):
+		weight = 200;
+		break;
+	 case /Light/.test(fontname):
+		weight = 300;
+		break;
+	 case /Regular/.test(fontname):
+		weight = 400;
+		break;
+	 case /Medium/.test(fontname):
+		weight = 500;
+		break;
+	 case /SemiBold/.test(fontname):
+		weight = 600;
+		break;
+	 case /Semi/.test(fontname):
+		weight = 600;
+		break;
+	 case /Bold/.test(fontname):
+		weight = 700;
+		break;
+	 case /ExtraBold/.test(fontname):
+		weight = 800;
+		break;
+	 case /Heavy/.test(fontname):
+		weight = 700;
+		break;
+	 case /Black/.test(fontname):
+		weight = 900;
+		break;
+	 default:
+		weight = 400;
+	}
+	return weight;
+}
+function fontsStyle(done) {
+  let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+  fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+  fs.readdir(path.build.fonts, function (err, items) {
+	 if (items) {
+		let c_fontname;
+		for (var i = 0; i < items.length; i++) {
+		  let fontname = items[i].split('.');
+		  fontname = fontname[0];
+		  let font = fontname.split('-')[0];
+		  let weight = checkWeight(fontname);
+		  if (c_fontname != fontname) {
+			 fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + font + '", "' + fontname + '", "' + weight + '" , "normal");\r\n', cb);
+		  }
+		  c_fontname = fontname;
+		}
+	 }
+	});
+	done();
+}
+*/
+
+function infofile() {
+
+}
 function cb() { }
 function clean() {
 	return del(path.clean);
@@ -206,15 +281,18 @@ function watchFiles() {
 	gulp.watch([path.watch.css], css);
 	gulp.watch([path.watch.js], js);
 	gulp.watch([path.watch.images], images);
+	gulp.watch([path.watch.json], json);
 }
-let build = gulp.series(clean, fonts_otf, gulp.parallel(html, css, js, favicon, images, videos), fonts, gulp.parallel(fontstyle));
+let build = gulp.series(clean, fonts_otf, gulp.parallel(html, css, js, json, favicon, images, videos), fonts, gulp.parallel(fontstyle, infofile));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 exports.html = html;
 exports.css = css;
 exports.js = js;
+exports.json = json;
 exports.videos = videos;
 exports.favicon = favicon;
+exports.infofile = infofile;
 exports.fonts_otf = fonts_otf;
 exports.fontstyle = fontstyle;
 exports.fonts = fonts;
